930ed2b5a13d89166fd10adf975c66f3f4c39b6f
ZatyAdnan <nurulizzatyadnan95@gmail.com>
2025-01-02 11:41:55 +0800

    commit

--- pages/logout/index.vue
+++ pages/logout/index.vue
@@ -1,21 +1,12 @@
 <script setup>
-import { useUserStore } from "~/stores/user";
+
 
 definePageMeta({
   title: "Logout",
   layout: "empty",
 });
 
-const userStore = useUserStore();
-
-await useFetch("/api/auth/logout", {
-  method: "GET",
-});
-
 if (process.client) {
-  userStore.setUsername("");
-  userStore.setRoles([]);
-  userStore.setIsAuthenticated(false);
 
   navigateTo("/login");
 }
